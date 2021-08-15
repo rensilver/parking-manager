9c85a27265359e1f0dd3a1d400100cc56b333c5a
rensilver <ren.oliv87@gmail.com>
2021-08-15 11:31:24 -0300

    Client entity and repository created

--- backend/src/main/java/com/rensilver/parkingmanager/entities/Client.java
+++ backend/src/main/java/com/rensilver/parkingmanager/entities/Client.java
@@ -1,4 +1,89 @@
 package com.rensilver.parkingmanager.entities;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_client")
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "firstName", nullable = false)
+    private String firstName;
+
+    @Column(name = "lastName", nullable = false)
+    private String lastName;
+
+    @Column(name = "cpf", nullable = false, unique = true)
+    private String cpf;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    public Client() {
+    }
+
+    public Client(Long id, String firstName, String lastName, String cpf, String address) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.cpf = cpf;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
